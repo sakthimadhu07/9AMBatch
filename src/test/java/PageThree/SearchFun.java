d4123a3b0ddbfb0096bb25eda1d0ca7a3b2af04d
sakthi madhu <ersakthi72@gmail.com>
2024-10-07 00:00:42 +0530

    modify

--- src/test/java/PageThree/SearchFun.java
+++ src/test/java/PageThree/SearchFun.java
@@ -7,8 +7,6 @@ public class SearchFun {
 	@Test
 	public void searchFun() {
 		System.out.println("SearchFun");
-		System.out.println("SearchFun");
-		System.out.println("sakthi");
 		System.out.println("sakthi");
 		System.out.println("sakthi");
 	}
